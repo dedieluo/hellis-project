5955b793fe0b469877ebcf4ed10207419ca687e7
dedieluo <112413687+dedieluo@users.noreply.github.com>
2023-04-18 12:48:04 +0300

    adding 5 pictures to the main page

--- frontend/src/Components/Layout/Content/Home/Home.tsx
+++ frontend/src/Components/Layout/Content/Home/Home.tsx
@@ -1,11 +1,37 @@
+import { NavLink, useNavigate } from "react-router-dom";
 import ImageAnimation from "../ImageAnimation/ImageAnimation";
 import "./Home.css";
 
 function Home(): JSX.Element {
+
     return (
         <div className="Home">
             <div className="imageAnimation">
                 <ImageAnimation />
+            </div>
+
+            <div className="main-picturs">
+                <NavLink to={"barMitzvah"}>
+                    <img src={require('../../images/בת מצווה.jpg')} alt="batMizva-picture" />
+                </NavLink>
+
+                <NavLink to={"oneYearOld"}>
+                    <img src={require('../../images/גיל שנה.jpg')} alt="oneYearOld-picture" />
+                </NavLink>
+
+                <NavLink to={"studio"}>
+                    <img src={require('../../images/סטודיו.jpg')} alt="studio-picture" />
+                </NavLink>
+            </div>
+
+            <div className="main-picturs">
+
+                <NavLink to={"image"}>
+                    <img src={require('../../images/תדמית.jpg')} alt="image-picture" />
+                </NavLink>
+                <NavLink to={"pregnancy"}>
+                    <img src={require('../../images/תמונת הריון ומשפחה.jpg')} alt="familyAndPregnancy-picture" />
+                </NavLink>
             </div>
         </div>
     );
--- frontend/src/Components/Layout/Navigate/Navigate.tsx
+++ frontend/src/Components/Layout/Navigate/Navigate.tsx
@@ -4,16 +4,16 @@ import "./Navigate.css";
 function Navigate(): JSX.Element {
     return (
         <div className="Navigate">
-            <NavLink to={"barMitzvah"}>בר מצווה</NavLink>
-            <NavLink to={"batMitzvah"}>בת מצווה</NavLink>
-            <NavLink to={"oneYearOld"}>גיל שנה</NavLink>
-            <NavLink to={"family"}>משפחה</NavLink>
-            <NavLink to={"pregnancy"}>הריון</NavLink>
+            <NavLink to={"contactUs"}>צור קשר</NavLink>
             <NavLink to={"image"}>תדמית</NavLink>
-            <NavLink to={"studio"}>סטודיו</NavLink>
             <NavLink to={"priceList"}>מחירון</NavLink>
             <NavLink to={"articles"}>מאמרים</NavLink>
-            <NavLink to={"contactUs"}>צור קשר</NavLink>
+            <NavLink to={"studio"}>סטודיו</NavLink>
+            <NavLink to={"pregnancy"}>הריון</NavLink>
+            <NavLink to={"oneYearOld"}>גיל שנה</NavLink>
+            <NavLink to={"family"}>משפחה</NavLink>
+            <NavLink to={"batMitzvah"}>בת מצווה</NavLink>
+            <NavLink to={"barMitzvah"}>בר מצווה</NavLink>
             <NavLink to={"home"}>דף הבית</NavLink>
 
         </div>
